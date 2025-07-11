d63a596eb4d0ce1619224856b890a1a8dbea6d31
oregon-selim-tuna <hselimtuna@gmail.com>
2025-07-12 00:43:48 +0300

    update: * HttpMessageNotReadableException added. * return format changed for handleValidationException.

--- services/platform-service/src/main/java/com/ecommerce/platform/exception/PlatformExceptionHandler.java
+++ services/platform-service/src/main/java/com/ecommerce/platform/exception/PlatformExceptionHandler.java
@@ -3,19 +3,23 @@ package com.ecommerce.platform.exception;
 import com.ecommerce.platform.dto.ErrorResponse;
 import com.ecommerce.platform.exception.platform.PlatformNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.List;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class PlatformExceptionHandler {
 
     @ExceptionHandler(PlatformNotFoundException.class)
-    public ResponseEntity<ErrorResponse>handlePlatformNotFound(PlatformNotFoundException ex, HttpServletRequest request){
+    public ResponseEntity<ErrorResponse>handlePlatformNotFound(@NotNull PlatformNotFoundException ex,
+                                                               @NotNull HttpServletRequest request){
         ErrorResponse errorResponse = new ErrorResponse(
                 LocalDateTime.now(),
                 request.getRequestURI(),
@@ -27,24 +31,49 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse>handleValidationException(MethodArgumentNotValidException ex, HttpServletRequest request){
+    public ResponseEntity<ErrorResponse> handleValidationException(@NotNull MethodArgumentNotValidException ex,
+                                                                   @NotNull HttpServletRequest request) {
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+        String errorMessage = String.join("; ", errors);
+
         ErrorResponse errorResponse = new ErrorResponse(
                 LocalDateTime.now(),
                 request.getRequestURI(),
                 HttpStatus.BAD_REQUEST.value(),
                 HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                ex.getMessage()
+                errorMessage
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse>handleInvalidJSON(
+            HttpMessageNotReadableException httpMessageNotReadableException,
+            @NotNull HttpServletRequest request){
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                request.getRequestURI(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "Invalid JSON Request"
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse>handleGenericException(Exception ex, HttpServletRequest request){
+    public ResponseEntity<ErrorResponse>handleGenericException(@NotNull Exception ex,
+                                                               @NotNull HttpServletRequest request){
         ErrorResponse errorResponse = new ErrorResponse(
                 LocalDateTime.now(),
                 request.getRequestURI(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
                 ex.getMessage()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);

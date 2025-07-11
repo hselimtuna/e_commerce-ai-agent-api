5b7120e4bb7c61f8ab8dd012a99b2f7c7f09364c
oregon-selim-tuna <hselimtuna@gmail.com>
2025-07-11 22:54:30 +0300

    update: ApiResponse renamed to CustomApiResponse to avoid import errors

--- services/platform-service/src/main/java/com/ecommerce/platform/response/CustomApiResponse.java
+++ services/platform-service/src/main/java/com/ecommerce/platform/response/CustomApiResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ApiResponse<T> {
+public class CustomApiResponse<T> {
     private boolean success;
     private String message;
     private T data;

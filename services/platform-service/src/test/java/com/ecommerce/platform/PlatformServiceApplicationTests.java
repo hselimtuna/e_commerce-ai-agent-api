d822ba8e1732440f8834760676e82a3ce9058abd
oregon-selim-tuna <hselimtuna@gmail.com>
2025-07-12 00:49:07 +0300

    update: async enabled.

--- services/platform-service/src/test/java/com/ecommerce/platform/PlatformServiceApplicationTests.java
+++ services/platform-service/src/test/java/com/ecommerce/platform/PlatformServiceApplicationTests.java
@@ -1,9 +1,11 @@
-package com.api.platform;
+package com.ecommerce.platform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootTest
+@EnableAsync
 class PlatformServiceApplicationTests {
 
 	@Test

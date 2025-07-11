526bc81d785ff3297a4034497843f5f835aca6fa
oregon-selim-tuna <hselimtuna@gmail.com>
2025-07-11 22:53:39 +0300

    create: custom api response for auth-service

---
+++ services/auth-service/src/main/java/com/ecommerce/auth/response/CustomApiResponse.java
@@ -0,0 +1,14 @@
+package com.ecommerce.auth.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+}

c94a936fcc0b09ddcfd814b246151c8f65e9da34
oregon-selim-tuna <hselimtuna@gmail.com>
2025-07-22 14:01:16 +0300

    update: test profile added

--- services/auth-service/src/test/java/com/ecommerce/auth/AuthApplicationTests.java
+++ services/auth-service/src/test/java/com/ecommerce/auth/AuthApplicationTests.java
@@ -2,12 +2,18 @@ package com.ecommerce.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.jpa.hibernate.ddl-auto=none",
+		"spring.datasource.initialization-mode=never"
+})
 class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }

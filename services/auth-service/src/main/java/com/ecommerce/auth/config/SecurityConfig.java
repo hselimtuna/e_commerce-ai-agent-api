f5d2987950ca779d428aba851542f558a5e77c9d
oregon-selim-tuna <hselimtuna@gmail.com>
2025-07-22 14:30:28 +0300

    update: redirect login added

--- services/auth-service/src/main/java/com/ecommerce/auth/config/SecurityConfig.java
+++ services/auth-service/src/main/java/com/ecommerce/auth/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.ecommerce.auth.config;
 
+import com.ecommerce.auth.handler.CustomOAuth2FailHandler;
+import com.ecommerce.auth.handler.CustomOAuth2SuccessHandler;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,10 +18,14 @@ public class SecurityConfig {
 
     @Bean(name = "securityDev")
     @Profile(value = "dev")
-    public SecurityFilterChain devSecurity(HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain devSecurity(@NotNull HttpSecurity httpSecurity,
+                                           CustomOAuth2SuccessHandler customOAuth2SuccessHandler,
+                                           CustomOAuth2FailHandler customOAuth2FailHandler) throws Exception {
         httpSecurity
-                .csrf(csrf -> csrf.disable())
-
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(customOAuth2SuccessHandler)
+                        .failureHandler(customOAuth2FailHandler))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**",
@@ -24,19 +33,17 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/api-docs/**",
                                 "/swagger-ui.html",
-                                "/webjars/**"
+                                "/webjars/**",
+                                "/oauth2/**"
                         )
-                        .permitAll().anyRequest().authenticated())
-
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable())
-
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers
-                        .xssProtection(xss -> xss.disable())
-                        .frameOptions(frame -> frame.disable()));
+                        .xssProtection(HeadersConfigurer.XXssConfig::disable)
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
         return httpSecurity.build();
     }
-
-
 }
